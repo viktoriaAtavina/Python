256605d1afee740e73dd56ebb6023c8b3dbf5355
Viktoria <viktoria.atavina@yandex.ru>
2023-01-17 22:09:14 +0300

    1

---
+++ 1.py
@@ -0,0 +1,7 @@
+# Напишите программу, удаляющую из текста все слова, содержащие "абв"
+
+text = text("Введите текст: ")
+
+del_word = "абв"
+= = [i for i in text.split(), if del_word not in i]
+print (" ".join (text))
